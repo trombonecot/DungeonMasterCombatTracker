20f07c56aaf086d3a39f4e5c80e87215670ca0d0
Eduard Cot Ros <eduardcot@iMac-de-Eduard.local>
2019-08-15 19:26:46 +0200

    basic usage

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,20 @@
 <script>
+    import { Button } from 'sveltestrap';
+	import Actor from './components/Actor.svelte';
 	export let name;
+
+	function random() {
+		return Math.floor((Math.random() * 10) + 1);
+	}
+
+	function handleClick() {
+		alert("hola");
+	}
 </script>
+
+<head>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
 
 <style>
 	h1 {
@@ -8,4 +22,10 @@
 	}
 </style>
 
-<h1>Hello {name}!</h1>
+<h1>Dungeon Master combat tracker!</h1>
+
+<Actor name="von Dersen" iniciative="{random()}"></Actor>
+<Actor name="Goblin 1" iniciative="{random()}"></Actor>
+<Actor name="Goblin 2" iniciative="{random()}"></Actor>
+
+<Button on:click="{handleClick}" >Next</Button> 
---
+++ src/components/Actor.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { Badge, Card } from 'sveltestrap';
+
+    export let name = 'NAME';
+    export let img;
+    export let iniciative = 0;
+    export let damage = 0;
+</script>
+
+<style>
+  div.damage {
+      background:red;
+      color:white;
+      padding:5px;
+      float:left;
+      width: 200px;
+  }
+</style>
+
+<Card>
+    <div>{name} - <Badge class="badge-success">{iniciative}</Badge></div>
+    <div class="damage">Damage recieved: {damage}</div>
+</Card>
+
